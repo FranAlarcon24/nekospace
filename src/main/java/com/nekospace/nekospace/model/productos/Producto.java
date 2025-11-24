03e8c88b372bf49d4b2e7d5489b4e54c4a644100
anti707 <ant.tapiao@duocuc.cl>
2025-11-24 20:47:34 -0300

    Revert "cambio model (ayuda)"
    This reverts commit 9d9cae3554ad535d55a9f4d28fe400afeca4a6cb.

--- src/main/java/com/nekospace/nekospace/model/productos/Producto.java
+++ src/main/java/com/nekospace/nekospace/model/productos/Producto.java
@@ -69,4 +69,11 @@ public class Producto {
     @JoinColumn(name = "producto_id")
     private List<Imagen> imagenes = new ArrayList<>();
 
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "producto_id")
+    private List<ProductosVenta> productosVenta = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "usuario_id")
+    private List<Usuario> usuarios = new ArrayList<>();
 }
