380141de59266462a969470d802292f88bb56a5f
anti707 <ant.tapiao@duocuc.cl>
2025-12-04 19:53:37 -0300

    Revert "evitar problema en cors"
    This reverts commit e75852170c8fad64dc01be7b3902a5ef1c527d6d.

--- src/main/java/com/nekospace/nekospace/config/WebConfig.java
+++ src/main/java/com/nekospace/nekospace/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import com.nekospace.nekospace.security.jwt.JwtAuthenticationFilter;
 
 
 
@@ -26,7 +25,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
                         .allowCredentials(true);
             }
