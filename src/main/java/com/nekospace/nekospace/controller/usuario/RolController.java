bc3d8bfc844d8f3f8db08766c872ed7c1113cec2
Roldemix <ma.nogueira@duocuc.cl>
2025-12-04 17:18:00 -0300

    vuelvieron en mayusculas

---
+++ src/main/java/com/nekospace/nekospace/controller/usuario/RolController.java
@@ -0,0 +1,78 @@
+package com.nekospace.nekospace.controller.usuario;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nekospace.nekospace.model.usuario.Rol;
+import com.nekospace.nekospace.service.usuario.RolService;
+
+@RestController
+@RequestMapping("/api/roles")
+public class RolController {
+
+    @Autowired
+    private RolService RolService;
+
+    @GetMapping
+    public ResponseEntity<List<Rol>> getAllRoles() {
+        List<Rol> roles = RolService.findAll();
+        if (roles.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(roles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rol> getRolById(@PathVariable Integer id) {
+        Rol rol = RolService.findById(id);
+        if (rol == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(rol);
+    }
+
+    @PostMapping
+    public ResponseEntity<Rol> createRol(@RequestBody Rol rol) {
+        Rol createdRol = RolService.save(rol);
+        return ResponseEntity.status(201).body(createdRol);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Rol> updateRol(@PathVariable Integer id, @RequestBody Rol rol) {
+        rol.setId(id);
+        Rol updatedRol = RolService.save(rol);
+        if (updatedRol == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedRol);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Rol> updatePartialRol(@PathVariable Integer id, @RequestBody Rol rol) {
+        rol.setId(id);
+        Rol updatedRol = RolService.partialUpdate(rol);
+        if (updatedRol == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(updatedRol);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteRol(@PathVariable Integer id) {
+        RolService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    //es para que me funcione hacer el commit
+}
