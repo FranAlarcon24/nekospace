314d060b4457facbed374d3d191b6f95e6de9037
anti707 <ant.tapiao@duocuc.cl>
2025-12-05 16:07:02 -0300

    Update WebConfig.java

--- src/main/java/com/nekospace/nekospace/config/WebConfig.java
+++ src/main/java/com/nekospace/nekospace/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
-                        .allowCredentials(true);
+                        ;
             }
         };
     }
