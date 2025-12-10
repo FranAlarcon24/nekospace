c364ea4fc98e6e328b66ebc78042b76cc2dbeafb
anti707 <ant.tapiao@duocuc.cl>
2025-12-10 13:26:22 -0300

    Update WebConfig.java

--- src/main/java/com/nekospace/nekospace/config/WebConfig.java
+++ src/main/java/com/nekospace/nekospace/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173,*")
+                        .allowedOrigins("http://localhost:5173,http://nekospace-frontend.vercel.app/,*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
                         ;
             }
